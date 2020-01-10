fe9eb9905bb31dc7214d0565f86ca9c93c6a9b99
Aitorlb7 <45202115+Aitorlb7@users.noreply.github.com>
2020-01-10 13:38:17 +0100

    Modules added

--- Aimbot_Freezer/Aimbot.h
+++ Aimbot_Freezer/Aimbot.h
@@ -1,5 +1,5 @@
 #include "SDL/include/SDL.h"
 
 
-void aimbot(Vec3d* shooter_pos, Vec3d* target_pos);
+void aimbot(Vec3d* shooter_pos, Vec3d* target_pos, float dt);
 bool collided(Vec3d* ball_pos, Vec3d* target_pos);
